584bf50796fb87979427b2431699396854455307
Sean Robbins <sean.m.robbins@gmail.com>
2013-07-28 15:23:59 +0100

    The Etsy Browse passing - this time it will actually click through onto a gallery.

--- features/pages/TreasuryPage.rb
+++ features/pages/TreasuryPage.rb
@@ -1,10 +1,10 @@
 class TreasuryPage < PageObject
 
   def chooseFirstGallery
-    @browser.div(:class => "item-treasury-info-box", :index => 0).click
+    @browser.div(:class => "item-treasury-info-box", :index => 0).link(:index => 0).click
   end
 
   def verifyTreasuryPageResults
-    @browser.div(:class, /primary index/).should exist
+    @browser.div(:class, /primary view/).should exist
   end
 end
